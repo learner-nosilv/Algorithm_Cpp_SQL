69053a51f3b7e552edf6f601d0fe0ccf6f86e5b0
nosilv <80045214+learner-nosilv@users.noreply.github.com>
2025-02-02 19:24:37 +0900

    [level 1] Title: 문자열 나누기, Time: 1.23 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    if(s.size()==1) return 1;
+    
+    vector<string> divided;
+    int i=0;
+    while(i<s.size()){
+        int eq=0; int neq=0;
+        for(int j=0; j<s.size(); j++){
+            if(s[i] == s[j]) eq++;
+            else neq++;
+            // 처음으로 두 횟수가 같아지는 순간
+            if(eq == neq){
+                divided.push_back(s.substr(0,j+1));
+                s=s.substr(j+1);
+                i=-1;
+                break;
+            }
+        }
+        i++;
+    }
+    if(s!="") divided.push_back(s);
+   
+    for(auto i : divided){
+        cout << i << ", ";
+    }
+    return divided.size();
+}
