6c86f6aec24f114b49cffc9e6eda0c727553ecb2
nosilv <80045214+learner-nosilv@users.noreply.github.com>
2025-02-05 20:42:19 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,15 +1,8 @@
 int solution(int n, int a, int b)
 {
-    // a < b 로 정렬
-    if(a>b){
-        int temp=a;
-        a=b;
-        b=temp;
-    }
-    
-    int count = 1;
+    int count = 0;
     while(true){
-        if(a%2!=0 && a+1==b) return count;
+        if(a==b) return count;
         a = (a+1)>>1;
         b = (b+1)>>1;
         count ++;
