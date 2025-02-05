062cb85f450ca433cbeb63e8bc7ac116a05f4a0e
nosilv <80045214+learner-nosilv@users.noreply.github.com>
2025-02-05 19:21:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,19 +1,10 @@
 #include <string>
-#include <vector>
-
 using namespace std;
 
 string solution(string s) {
-    bool isFirst = true;
-    for(int i=0; i<s.size(); i++){
-        if(isFirst==true){
-            if(s[i] >= 'a' && s[i] <= 'z')
-                s[i] = s[i]-'a'+'A';
-            isFirst=false;
-        }
-        else if(s[i]>='A' && s[i]<='Z')
-            s[i] = s[i]-'A'+'a';
-        if(s[i]==' ') isFirst=true;
+    s[0] = toupper(s[0]);
+    for(int i=1; i<s.size(); i++){
+        s[i] = (s[i-1] == ' ' ? toupper(s[i]) : tolower(s[i]));
     }
     return s;
 }
